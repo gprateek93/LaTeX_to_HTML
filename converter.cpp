2dd7d473ea4c315dedc87610ae3e3de6c8ba5a85
gprateek93 <gprateek93@gmail.com>
2018-09-01 03:50:12 +0530

    Adding new converter function

--- converter.cpp
+++ converter.cpp
@@ -2,7 +2,6 @@
 #include "converter.h"
 #include <iostream>
 #include <fstream>
-#include <set>
 #include <string>
 #include <sstream>
 
@@ -28,7 +27,7 @@ converter :: converter(){
     myMapping[5] = "strong";
     myMapping[6] = "em";
     myMapping[7] = "u";
-    myMapping[8] = "p";
+    myMapping[8] = "br";
     myMapping[9] = "";
     myMapping[10] = "";
     myMapping[11] = "table";
@@ -39,7 +38,82 @@ converter :: converter(){
     myMapping[16] = "body";
     myMapping[17] = "tr";
     myMapping[18] = "td";
-    emptyTags.insert(13);
+}
+
+string converter :: traverseSection(ast_node * root, int type){
+    section_no++;
+    subsection_no = 0;
+    string s = "";
+    s+="<"+getMapping(type)+">";
+    s+=myString(section_no)+" "+root->data;
+    s+="</"+getMapping(type)+">";
+    s+=traverseChildren (root);
+    return s;
+}
+
+string converter :: traverseSubSection(ast_node * root, int type){
+    subsection_no++;
+    string s = "";
+    s+="<"+getMapping(type)+">";
+    s+=myString(section_no)+"."+myString(subsection_no)+" "+root->data;
+    s+="</"+getMapping(type)+"/>";
+    s+=traverseChildren(root);
+    return s;
+}
+
+string converter :: traverseList(ast_node * root, int type){
+    return traverseDefault(root,type);
+}
+
+string converter :: traverseFont(ast_node * root, int type){
+    return traverseDefault(root,type);
+}
+string converter :: traversePara(ast_node * root, int type){
+    string s = "";
+    s+="<"+getMapping(type)+">";
+    s+="&nbsp;&nbsp;&nbsp;&nbsp;";
+    s+=traverseChildren(root);
+    return s;
+}
+string converter :: traverseAnchor(ast_node * root, int type){
+    string s = "";
+    return s;
+}
+
+string converter :: traverseTable(ast_node * root, int type){
+    return traverseDefault(root,type);
+}
+
+string converter :: traverseImage(ast_node * root, int type){
+    string s = "";
+    if(type == 13){
+        s+="<"+ getMapping(type)+" src = "+"\""+root->data+"\" alt = \"Image Here\" "+root->attributes+">";
+        s+=traverseChildren(root);
+    }
+    else{
+        s+=traverseDefault(root,type);
+    }
+    return s;
+}
+
+string converter :: traverseContent(ast_node * root, int type){
+    return root->data;
+}
+
+string converter :: traverseDefault(ast_node * root, int type){
+    string s = "";
+    s+="<"+getMapping(type)+">";
+    s+=traverseChildren(root);
+    s+="</"+getMapping(type)+">";
+    return s;
+}
+
+string converter :: traverseChildren(ast_node * root){
+    string s = "";
+    for(int i = 0; i<root->children.size(); i++){
+        s+=traversal(root->children[i]);
+    }
+    return s;
 }
 
 string converter :: traversal(ast_node *root){
@@ -48,8 +122,48 @@ string converter :: traversal(ast_node *root){
         int type = root->node_type;
         string attributes = root->attributes;
         string data = root->data;
+        switch (type) {
+            case 0:
+                s+= traverseSection(root,type);
+                break;
+            case 1:
+                s+=traverseSubSection(root,type);
+                break;
+            case 2:
+            case 3:
+            case 4:
+                s+=traverseList(root,type);
+                break;
+            case 5:
+            case 6:
+            case 7:
+                s+=traverseFont(root,type);
+                break;
+            case 8:
+                s+=traversePara(root,type);
+                break;
+            case 9:
+            case 10:
+                s+=traverseAnchor(root,type);
+                break;
+            case 11:
+            case 17:
+            case 18:
+                s+=traverseTable(root,type);
+                break;
+            case 12:
+            case 13:
+            case 14:
+                s+=traverseImage(root,type);
+                break;
+            case 15:
+                s+=traverseContent(root,type);
+                break;
+            default:
+                s+=traverseDefault(root,type);
+        }
         //opening
-        if(attributes.empty()){
+        /*if(attributes.empty()){
             //cout<<f
             if(type!=15){
                 s += "<"+myMapping[type]+">";
@@ -94,8 +208,13 @@ string converter :: traversal(ast_node *root){
         else{
             s+=">";
         }
+    }*/
     }
     return s;
+ }
+
+string converter :: getMapping(int type){
+    return myMapping[type];
 }
 
 void converter :: printHTML(string s){
--- converter.h
+++ converter.h
@@ -3,7 +3,6 @@
 
 #include "ast.h"
 #include <map>
-#include <set>
 #include <string>
 using namespace std;
 
@@ -11,11 +10,22 @@ using namespace std;
 class converter{
     private:
         map<int,string> myMapping;
-        set<int> emptyTags;
     public:
         converter();
         string traversal(ast_node * root);
+        string traverseSection(ast_node *root, int type);
+        string traverseSubSection(ast_node *root, int type);
+        string traverseList(ast_node *root, int type);
+        string traverseAnchor(ast_node *root, int type);
+        string traversePara(ast_node *root, int type);
+        string traverseFont(ast_node *root, int type);
+        string traverseTable(ast_node *root, int type);
+        string traverseImage(ast_node *root, int type);
+        string traverseDefault(ast_node *root, int type);
+        string traverseContent(ast_node *root, int type);
+        string traverseChildren(ast_node *root);
         void printHTML(string s);
+        string getMapping(int type);
 };
 
 #endif
